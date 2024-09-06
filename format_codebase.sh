5cb19697d193048da7b5ce232a6f841d3b707d79
Salmaan Saeed <salsaeed999@gmail.com>
2024-09-06 12:44:04 -0400

    fixed the clang-format and clang-tidy scripts

--- format_codebase.sh
+++ format_codebase.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cpp_files=$(find src/ -name "*cpp")
 hpp_files=$(find src/ -name "*hpp")
-include_hpp_files=$(find include/ -name "*hpp")
-clang-format --style=file -i $cpp_files $hpp_files $include_hpp_files
+clang-format --style=file -i $cpp_files $hpp_files
--- lint_codebase.sh
+++ lint_codebase.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cpp_files=$(find ../src/ -name "*cpp")
 hpp_files=$(find ../src/ -name "*hpp")
-include_hpp_files=$(find ../include/ -name "*hpp")
-clang-tidy --format-style=file $cpp_files $hpp_files $include_hpp_files
+clang-tidy --format-style=file $cpp_files $hpp_files
